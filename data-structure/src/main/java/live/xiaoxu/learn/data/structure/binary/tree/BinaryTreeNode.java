7b72ba4dd7e2a960c055b0e693973091024f7a01
小徐 <x@xiaoxu.live>
2022-12-12 14:43:55 +0800

    添加数据结构

---
+++ data-structure/src/main/java/live/xiaoxu/learn/data/structure/binary/tree/BinaryTreeNode.java
@@ -0,0 +1,30 @@
+package live.xiaoxu.learn.data.structure.binary.tree;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Xiao Xu
+ * @date 2021/9/28 下午 02:37
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BinaryTreeNode {
+
+    /**
+     * 数据
+     */
+    private Object data;
+
+    /**
+     * 左孩子
+     */
+    private BinaryTreeNode leftChild;
+
+    /**
+     * 右孩子
+     */
+    private BinaryTreeNode rightChild;
+}
