58d3eb2b42aeed77a56b8256f9cc099f8a992f63
小徐 <x@xiaoxu.live>
2023-02-08 13:41:41 +0800

    调整

---
+++ leetcode/src/main/java/live/xiaoxu/leetcode/No001757.java
@@ -0,0 +1,15 @@
+package live.xiaoxu.leetcode;
+
+/**
+ * <p><a href="https://leetcode.cn/problems/recyclable-and-low-fat-products/">1757. 可回收且低脂的产品</a></p>
+ *
+ * @author 小徐
+ * @since 2023/2/8 10:58
+ */
+public class No001757 {
+
+    public static void main(String[] args) {
+
+        // SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+    }
+}
