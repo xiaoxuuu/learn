1470afaeeb061eff1311c6aa32733c9b964595e7
小徐 <xuhaoran@live.com>
2023-03-22 08:58:44 +0800

    Unicode

---
+++ basic-jdk/src/main/java/live/xiaoxu/jdk/unicode/Unicode.java
@@ -0,0 +1,25 @@
+package live.xiaoxu.jdk.unicode;
+
+public class Unicode {
+
+    public static void main(String[] args) {
+
+        // 截取前两个字符
+        String str = "这是☁️云朵";
+        System.out.println(subStr(str, 4));
+        System.out.println(subStr(str, 3));
+        System.out.println(subStr(str, 2));
+        System.out.println(subStr(str, 1));
+    }
+
+    private static String subStr(String str, int length) {
+
+        int codePointCount = str.codePointCount(0, str.length());
+        if (codePointCount > length) {
+            // 获取包含 emoji 在内的两个字符实际长度
+            int offset = str.offsetByCodePoints(0, length);
+            return str.substring(0, offset);
+        }
+        return str;
+    }
+}
