59af3867b643c9f5d2ff9d0fb651d03bc917e7fe
小徐 <xuhaoran@live.com>
2023-05-12 14:51:41 +0800

    优化解法

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000003.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000003.java
@@ -12,6 +12,32 @@ import java.util.Set;
 public class No000003 {
 
     public int lengthOfLongestSubstring(String s) {
+        // 记录字符上一次出现的位置
+        int[] last = new int[128];
+        for (int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        // 窗口开始位置
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+        return res;
+    }
+
+    /**
+     * 愚笨的方法，
+     *
+     * @param s 数据
+     * @return 结果
+     */
+    public int lengthOfLongestSubstringMy(String s) {
         char[] charArray = s.toCharArray();
         Set<Character> characterSet = new HashSet<>();
         for (char c : charArray) {
