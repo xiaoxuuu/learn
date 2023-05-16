5cd5d91d527919e41db05044f03f99bf113461ea
小徐 <xuhaoran@live.com>
2023-05-16 14:25:17 +0800

    5

---
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000005.java
@@ -0,0 +1,48 @@
+package live.xiaoxu.leetcode;
+
+import java.util.Stack;
+
+/**
+ * <p><a href="https://leetcode.cn/problems/longest-palindromic-substring/">5. 最长回文子串</a></p>
+ *
+ * @author 小徐
+ * @since 2023/5/16 10:57
+ */
+public class No000005 {
+
+    public String longestPalindrome(String s) {
+
+        int length = s.length();
+        if (length == 1) {
+            return s;
+        }
+        char[] charArray = s.toCharArray();
+
+        int begin = 0, end = 0;
+        // 循环每一次
+        for (int i = 0; i < length; i++) {
+
+            // 已知子串长度超过字符串一半，则剩下的一半无需判断
+            if (s.length() < (end - begin) * 2) {
+                break;
+            }
+            for (int j = i + 1; j < length; j++) {
+                boolean choose = true;
+                Stack<Character> stack = new Stack<>();
+                int tempJ = j;
+                for (int k = i; k <= j; k++) {
+                    if (charArray[k] != charArray[tempJ]) {
+                        choose = false;
+                        break;
+                    }
+                    tempJ--;
+                }
+                if (choose && j - i > end - begin) {
+                    begin = i;
+                    end = j;
+                }
+            }
+        }
+        return s.substring(begin, end + 1);
+    }
+}
