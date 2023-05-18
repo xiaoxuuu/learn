d0e3138afc6e26be04d53786acdbcc88aa0e0839
小徐 <xuhaoran@live.com>
2023-05-18 18:02:47 +0800

    优化

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000006.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000006.java
@@ -1,5 +1,8 @@
 package live.xiaoxu.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p><a href="https://leetcode.cn/problems/zigzag-conversion/">6. N 字形变换</a></p>
  *
@@ -9,13 +12,66 @@ package live.xiaoxu.leetcode;
 public class No000006 {
 
     /**
-     * 暴力法，将每个字符放置到对应位置，再遍历
+     * <p>规律法：</p>
+     * <p>2 行：</p>
+     * <pre>0 2 4 6 8 10 12 14 16 18</pre>
+     * <pre>1 3 5 7 9 11 13 15 17</pre>
+     * <p>3 行：</p>
+     * <pre>0   4   8     12    16</pre>
+     * <pre>1 3 5 7 9  11 13 15 17</pre>
+     * <pre>2   6   10    14    18</pre>
+     * <p>4 行：</p>
+     * <pre>0     6      12        18</pre>
+     * <pre>1   5 7   11 13    17</pre>
+     * <pre>2 4   8 10   14 16</pre>
+     * <pre>3     9      15</pre>
      *
      * @param s       字符串
      * @param numRows 所需行
      * @return 结果
      */
     public String convert(String s, int numRows) {
+
+        int length = s.length();
+        if (length <= 1 || numRows == 1) {
+            return s;
+        }
+
+        // 所有字符
+        char[] charArray = s.toCharArray();
+        List<Character>[] listArray = new ArrayList[numRows];
+        for (int i = 0; i < listArray.length; i++) {
+            listArray[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < charArray.length; i++) {
+            int k = i % (2 * numRows - 2);
+            if (k <= numRows - 1) {
+                // s[i] 就属于第 k 行
+                listArray[k].add(charArray[i]);
+            } else {
+                // s[i] 就属于 2n-2-k 行
+                listArray[2 * numRows - 2 - k].add(charArray[i]);
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (List<Character> characters : listArray) {
+            for (Character character : characters) {
+                stringBuilder.append(character);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 暴力法，将每个字符放置到对应位置，再遍历
+     *
+     * @param s       字符串
+     * @param numRows 所需行
+     * @return 结果
+     */
+    public String convertOne(String s, int numRows) {
 
         if (s.length() <= 1 || numRows == 1) {
             return s;
