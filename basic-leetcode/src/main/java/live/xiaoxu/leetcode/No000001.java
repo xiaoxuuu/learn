559dc7f3d880d9e6a1b45183fcd624ede5177e26
小徐 <xuhaoran@live.com>
2023-05-19 10:37:24 +0800

    Hash 法

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000001.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000001.java
@@ -1,5 +1,7 @@
 package live.xiaoxu.leetcode;
 
+import java.util.HashMap;
+
 /**
  * <p><a href="https://leetcode.cn/problems/two-sum/">1. 两数之和</a></p>
  *
@@ -9,6 +11,20 @@ package live.xiaoxu.leetcode;
 public class No000001 {
 
     public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{i, map.get(result)};
+            }
+            map.put(nums[i], i);
+            map.put(-result, i);
+        }
+        return null;
+    }
+
+    public int[] twoSumOen(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++) {
             int sub = target - nums[i];
