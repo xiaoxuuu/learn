1def707ca35c34c4422baa4cab4563fd0d143cb9
小徐 <xuhaoran@live.com>
2023-05-25 17:23:24 +0800

    优化

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002451.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002451.java
@@ -15,25 +15,23 @@ public class No002451 {
         }
         int[] intArray = new int[words[0].length() - 1];
 
-        char[] charArray = words[0].toCharArray();
-        for (int i = 0; i < charArray.length - 1; i++) {
-            intArray[i] = charArray[i + 1] - charArray[i];
+        for (int i = 0; i < words[0].length() - 1; i++) {
+            intArray[i] = words[0].charAt(i + 1) - words[0].charAt(i);
         }
 
         // 与第 0 相同的字符串数量
-        int sameStrNum = 0;
+        int equalStrNum = 0;
         // 不与第 0 相同的字符串索引位置
         int indexNotEqual = -1;
-        boolean sameStrNumAdd;
+        boolean equalStrNumAdd;
         for (int i = 1; i < words.length; i++) {
-            sameStrNumAdd = true;
-            char[] charArrayTemp = words[i].toCharArray();
-            for (int j = 0; j < charArrayTemp.length - 1; j++) {
-                int tempSub = charArrayTemp[j + 1] - charArrayTemp[j];
+            equalStrNumAdd = true;
+            for (int j = 0; j < words[i].length() - 1; j++) {
+                int tempSub = words[i].charAt(j + 1) - words[i].charAt(j);
                 // 遇到与第 0 个不同的字符串
                 if (intArray[j] != tempSub) {
                     // 已有与第 0 相同字符串，返回当前
-                    if (sameStrNum != 0) {
+                    if (equalStrNum != 0) {
                         return words[i];
                     }
                     // 已有与第 0 不同字符串，返回第 0
@@ -41,12 +39,12 @@ public class No002451 {
                         return words[0];
                     }
                     indexNotEqual = i;
-                    sameStrNumAdd = false;
+                    equalStrNumAdd = false;
                     break;
                 }
             }
-            if (sameStrNumAdd) {
-                sameStrNum++;
+            if (equalStrNumAdd) {
+                equalStrNum++;
             }
         }
         return words[indexNotEqual];
--- basic-leetcode/src/test/java/live/xiaoxu/leetcode/No002451Test.java
+++ basic-leetcode/src/test/java/live/xiaoxu/leetcode/No002451Test.java
@@ -10,8 +10,11 @@ class No002451Test {
 
         No002451 n2451 = new No002451();
 
-        Assertions.assertEquals("ddd", n2451.oddString(new String[]{"ddd", "poo", "baa", "onn"}));
+        Assertions.assertEquals("aas", n2451.oddString(new String[]{"aaa", "aas", "bbb"}));
+        Assertions.assertEquals("abd", n2451.oddString(new String[]{"abd", "dfg", "kmn"}));
         Assertions.assertEquals("bob", n2451.oddString(new String[]{"aaa", "bob", "ccc", "ddd"}));
+        Assertions.assertEquals("ddd", n2451.oddString(new String[]{"dcc", "poo", "baa", "onn", "ddd"}));
+        Assertions.assertEquals("ddd", n2451.oddString(new String[]{"ddd", "poo", "baa", "onn"}));
         Assertions.assertEquals("abc", n2451.oddString(new String[]{"adc", "wzy", "abc"}));
     }
 }
