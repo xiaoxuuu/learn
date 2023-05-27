da045eecfa10d329e10bba3fa30ce994f323570d
小徐 <xuhaoran@live.com>
2023-05-27 13:44:37 +0800

    优化

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No001093.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No001093.java
@@ -3,7 +3,6 @@ package live.xiaoxu.leetcode;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * <p><a href="https://leetcode.cn/problems/statistics-from-a-large-sample/">1093. 大样本统计</a></p>
@@ -20,7 +19,7 @@ public class No001093 {
         // 最大数（数组索引）
         double maximum = Double.MIN_VALUE;
         // 平均值（数组索引 * 数据）/ 总数
-        double mean;
+        double mean = 0D;
         // 中位数（数据）
         double median = 0D;
         // 众数（数组索引）
@@ -29,7 +28,7 @@ public class No001093 {
         int maxShowTimes = 0;
         // 总共出现的数字个数
         long allTimes = 0;
-        Map<Integer, Integer> map = new LinkedHashMap<>();
+        LinkedHashMap<Integer, Long> map = new LinkedHashMap<>();
         for (int i = 0; i < count.length; i++) {
             if (count[i] == 0) {
                 continue;
@@ -44,22 +43,23 @@ public class No001093 {
                 mode = i;
             }
             allTimes += count[i];
-            map.put(i, count[i]);
+            map.put(i, (long) count[i]);
         }
 
         // 平均数
-        mean = map.entrySet().stream().map(k -> (long) k.getKey() * (long) k.getValue()).collect(Collectors.summarizingLong(value -> value)).getSum();
+        for (Map.Entry<Integer, Long> entry : map.entrySet()) {
+            mean += entry.getKey() * entry.getValue();
+        }
 
         // 中位数位置
         long mapHalfSize = allTimes / 2;
-        boolean addNext = allTimes % 2 == 0;
         long index = 0;
-        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        Iterator<Map.Entry<Integer, Long>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
-            Map.Entry<Integer, Integer> entry = iterator.next();
+            Map.Entry<Integer, Long> entry = iterator.next();
             index += entry.getValue();
             if (mapHalfSize == index) {
-                if (addNext) {
+                if (allTimes % 2 == 0) {
                     median = (double) (entry.getKey() + iterator.next().getKey()) / 2;
                 } else {
                     median = iterator.next().getKey();
