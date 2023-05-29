dee890d59798897974e9a6498c585d9d5893980c
小徐 <xuhaoran@live.com>
2023-05-29 11:04:55 +0800

    620

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000620_1.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000620_1.java
@@ -6,5 +6,7 @@ package live.xiaoxu.leetcode;
  * @author 小徐
  * @since 2023/5/19 10:07
  */
-public class No000620_1_ {
+public class No000620_1 {
+
+    // SELECT * FROM cinema WHERE description != 'boring' AND id % 2 != 0 ORDER BY rating DESC;
 }
