bc0a68d72f8de9dfc3e9afc4ddc8b95fb830fd6c
小徐 <xuhaoran@live.com>
2023-05-29 10:25:34 +0800

    重命名

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002455.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002455.java
@@ -11,13 +11,13 @@ public class No002455 {
     public int averageValue(int[] nums) {
 
         // 总数、和
-        int total = 0, sum = 0;
+        int count = 0, sum = 0;
         for (int num : nums) {
             if (num % 6 == 0) {
                 sum += num;
-                total++;
+                count++;
             }
         }
-        return total == 0 ? total : sum / total;
+        return count == 0 ? count : sum / count;
     }
 }
