60edf55bf93dc72af9f7c61f468403e62ad73d51
小徐 <xuhaoran@live.com>
2023-06-02 13:57:38 +0800

    前缀和

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002559.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No002559.java
@@ -1,59 +1,34 @@
 package live.xiaoxu.leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * <p><a href="https://leetcode.cn/problems/count-vowel-strings-in-ranges/">2559. 统计范围内的元音字符串数</a></p>
+ * <p>前缀和</p>
  *
  * @author 小徐
  * @since 2023/6/2 10:27
  */
 public class No002559 {
 
-    private static final Set<Character> SET = new HashSet<>() {{
-        add('a');
-        add('e');
-        add('i');
-        add('o');
-        add('u');
-    }};
+    private static boolean isVowel(char c) {
 
-    private final Map<String, Boolean> verifiedMap = new HashMap<>();
-    private final Map<String, Integer> verifiedArrayMap = new HashMap<>();
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
 
     public int[] vowelStrings(String[] words, int[][] queries) {
 
         // 判定所有数据
-        int[] qualified = new int[words.length];
+        int[] qualified = new int[words.length + 1];
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
-            if (verifiedMap.containsKey(word)) {
-                qualified[i] = verifiedMap.get(word) ? 1 : 0;
-                continue;
-            }
-            boolean b = SET.contains(word.charAt(0)) && SET.contains(word.charAt(word.length() - 1));
-            verifiedMap.put(word, b);
-            qualified[i] = b ? 1 : 0;
+            boolean vowel = isVowel(word.charAt(0)) && isVowel(word.charAt(word.length() - 1));
+            qualified[i + 1] = qualified[i] + (vowel ? 1 : 0);
         }
 
         // 判定指定范围
-        // TODO 优化时间
         int[] retQualifiedNum = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int[] query = queries[i];
-            if (verifiedArrayMap.containsKey(query[0] + "-" + query[1])) {
-                retQualifiedNum[i] = verifiedArrayMap.get(query[0] + "-" + query[1]);
-                continue;
-            }
-            int ret = 0;
-            for (int j = query[0]; j <= query[1]; j++) {
-                ret += qualified[j];
-            }
-            retQualifiedNum[i] = ret;
-            verifiedArrayMap.put(query[0] + "-" + query[1], ret);
+            retQualifiedNum[i] = qualified[query[1] + 1] - qualified[query[0]];
         }
         return retQualifiedNum;
     }
--- basic-leetcode/src/test/java/live/xiaoxu/leetcode/No002559Test.java
+++ basic-leetcode/src/test/java/live/xiaoxu/leetcode/No002559Test.java
@@ -10,7 +10,7 @@ class No002559Test {
 
         No002559 n2559 = new No002559();
 
-        Assertions.assertArrayEquals(new int[]{3, 2, 1}, n2559.vowelStrings(new String[]{"a", "e", "i"}, new int[][]{{0, 2}, {0, 1}, {2, 2}}));
         Assertions.assertArrayEquals(new int[]{2, 3, 0}, n2559.vowelStrings(new String[]{"aba", "bcb", "ece", "aa", "e"}, new int[][]{{0, 2}, {1, 4}, {1, 1}}));
+        Assertions.assertArrayEquals(new int[]{3, 2, 1}, n2559.vowelStrings(new String[]{"a", "e", "i"}, new int[][]{{0, 2}, {0, 1}, {2, 2}}));
     }
 }
