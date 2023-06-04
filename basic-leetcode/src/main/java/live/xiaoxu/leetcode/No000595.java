910ee8527ba3291cc11510c23306cfc58342583d
小徐 <xuhaoran@live.com>
2023-06-04 10:52:56 +0800

    595

--- basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000595.java
+++ basic-leetcode/src/main/java/live/xiaoxu/leetcode/No000595.java
@@ -6,6 +6,7 @@ package live.xiaoxu.leetcode;
  * @author 小徐
  * @since 2023/5/19 10:07
  */
-public class No000595_ {
+public class No000595 {
 
+    // SELECT name, population, area FROM World WHERE area >= 3000000 OR population >= 25000000;
 }
