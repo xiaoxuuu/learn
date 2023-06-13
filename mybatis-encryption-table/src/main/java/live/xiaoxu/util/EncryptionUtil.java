c355673eba4cccbc764b9800040d0507f6fdfd8a
小徐 <xuhaoran@live.com>
2023-06-13 11:16:15 +0800

    调整映射

--- mybatis-encryption-table/src/main/java/live/xiaoxu/util/EncryptionUtil.java
+++ mybatis-encryption-table/src/main/java/live/xiaoxu/util/EncryptionUtil.java
@@ -5,15 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class EncryptionUtil {
 
-    private static final String confusionVariable = "0AAB595D13E70AA81A9B52DBE69F922E";
+    private static final String CONFUSION_VARIABLE = "0B4FD093BD6A97154001542E682A9289";
 
-    public static String encryptionData(String data) {
+    public static String encryptionData(Object data) {
 
-        String md5 = MD5Util.getMD5(data + confusionVariable);
+        String md5 = MD5Util.getMD5(data.toString() + CONFUSION_VARIABLE);
         StringBuilder stringBuilder = new StringBuilder();
         for (char c : md5.toCharArray()) {
-            if (c < 58 && 47 < c) {
-                stringBuilder.append((char) (c + 22));
+            if (48 <= c && c <= 57) {
+                // 0 ~ 9 映射为 G ～ P
+                stringBuilder.append((char) (c + 23));
             } else {
                 stringBuilder.append(c);
             }
