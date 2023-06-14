2698f4080e0a221fa04a5450e1763bf792b0655c
小徐 <xuhaoran@live.com>
2023-06-14 17:06:25 +0800

    解析工具

--- mybatis-encryption-table/src/main/java/live/xiaoxu/controller/EncryptionController.java
+++ mybatis-encryption-table/src/main/java/live/xiaoxu/controller/EncryptionController.java
@@ -3,7 +3,9 @@ package live.xiaoxu.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import live.xiaoxu.entity.SQLDTO;
 import live.xiaoxu.service.EncryptionService;
+import live.xiaoxu.util.EncodeUtil;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "编码", description = "描述")
@@ -21,5 +23,20 @@ public class EncryptionController {
     void encryptionDatabase() {
 
         encryptionService.encryptionDatabase();
+    }
+
+    /**
+     * <p>使用方式：</p>
+     * <p>{@code curl -X POST 'http://localhost:8888/encryption/decode_sql' -H 'Content-Type: application/json' --data-raw '{"sql":"待解码 SQL"}'}</p>
+     *
+     * @param sqlDto sql
+     * @return sql
+     */
+    @Operation(summary = "解密数据", description = "解密被加密的 SQL")
+    @PostMapping("/decode_sql")
+    public @ResponseBody
+    String decodeSql(@RequestBody SQLDTO sqlDto) {
+
+        return EncodeUtil.decodeData(sqlDto.getSql());
     }
 }
--- mybatis-encryption-table/src/main/java/live/xiaoxu/util/EncodeUtil.java
+++ mybatis-encryption-table/src/main/java/live/xiaoxu/util/EncodeUtil.java
@@ -2,25 +2,49 @@ package live.xiaoxu.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 public class EncodeUtil {
 
     private static final String CONFUSION_VARIABLE = "0B4FD093BD6A97154001542E682A9289";
 
+    private static final Map<String, String> ENCODE_MAP = new HashMap<>();
+    private static final Map<String, String> DECODE_MAP = new HashMap<>();
+
     public static String encodeData(Object data) {
 
-        String md5 = MD5Util.getMD5(data.toString() + CONFUSION_VARIABLE);
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char c : md5.toCharArray()) {
-            if (48 <= c && c <= 57) {
-                // 0 ~ 9 映射为 G ～ P
-                stringBuilder.append((char) (c + 23));
-            } else {
-                stringBuilder.append(c);
+        String encodeString;
+        if (!ENCODE_MAP.containsKey(data.toString())) {
+            String md5 = MD5Util.getMD5(data.toString() + CONFUSION_VARIABLE);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char c : md5.toCharArray()) {
+                if (48 <= c && c <= 57) {
+                    // 0 ~ 9 映射为 G ～ P
+                    stringBuilder.append((char) (c + 23));
+                } else {
+                    stringBuilder.append(c);
+                }
             }
+            encodeString = stringBuilder.toString();
+            ENCODE_MAP.put(data.toString(), encodeString);
+            DECODE_MAP.put(encodeString, data.toString());
+        } else {
+            encodeString = ENCODE_MAP.get(data.toString());
         }
 
-        log.debug("原数据：{}，编码后数据：{}", data, stringBuilder);
-        return stringBuilder.toString();
+        log.debug("原数据：{}，编码后数据：{}，编码规则：md5(data + {})", data, encodeString, CONFUSION_VARIABLE);
+        return encodeString;
+    }
+
+    public static String decodeData(String data) {
+
+        for (String k : DECODE_MAP.keySet()) {
+            if (data.contains(k)) {
+                data = data.replace(k, DECODE_MAP.get(k));
+            }
+        }
+        return data;
     }
 }
