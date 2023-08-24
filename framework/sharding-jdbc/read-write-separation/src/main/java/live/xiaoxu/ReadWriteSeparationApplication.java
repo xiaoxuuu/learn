9e376e6617df3a7c67c7e8e45c2e02acf32aa1fa
小徐 <xuhaoran@live.com>
2023-08-24 13:35:38 +0800

    新增启动类

---
+++ framework/sharding-jdbc/read-write-separation/src/main/java/live/xiaoxu/ReadWriteSeparationApplication.java
@@ -0,0 +1,16 @@
+package live.xiaoxu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "live.xiaoxu")
+@MapperScan("live.xiaoxu")
+public class ReadWriteSeparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadWriteSeparationApplication.class, args);
+    }
+}
