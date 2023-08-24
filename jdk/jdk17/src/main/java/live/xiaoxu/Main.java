7c6677c90cebc5b02e16318864e675c683099ba6
小徐 <xuhaoran@live.com>
2023-08-24 10:13:51 +0800

    添加数据

---
+++ jdk/jdk17/src/main/java/live/xiaoxu/Main.java
@@ -0,0 +1,11 @@
+package live.xiaoxu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("java.vendor.url"));
+    }
+}
