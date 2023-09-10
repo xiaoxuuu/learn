8215e6a2c5f30825cf568531b7150a3d37d9abfe
小徐 <xuhaoran@live.com>
2023-09-10 17:26:12 +0800

    新建 xxl-job 执行器

---
+++ framework/springboot/xxl-job/src/main/java/live/xiaoxu/XxlJobMain.java
@@ -0,0 +1,12 @@
+package live.xiaoxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobMain.class, args);
+    }
+}
